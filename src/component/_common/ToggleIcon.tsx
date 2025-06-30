b3d3d672663b29a31fb8239fb36178357168f136
tsulocalize <tsulocalizing@gmail.com>
2025-06-30 15:56:07 +0900

    refactor: icon / toogleIcon 분리

--- src/component/_common/ToggleIcon.tsx
+++ src/component/_common/ToggleIcon.tsx
@@ -1,33 +1,35 @@
 import styled from "styled-components";
-import React from "react";
+import React, {useState} from "react";
 import {useSizeStore} from "@/store/useSizeStore.ts";
 
 interface Props {
   svg: React.FC<React.SVGProps<SVGSVGElement>>,
-  onClick?: () => void,
+  onClick?: () => void
 }
 
-export const Icon = ({svg, onClick}: Props) => {
+export const ToggleIcon = ({svg, onClick}: Props) => {
+  const [active, setActive] = useState(false);
   const { ratio } = useSizeStore();
 
   const handleClick = () => {
+    setActive(!active);
     if (onClick) {
       onClick();
     }
   }
 
   return (
-    <S.StyledIcon as={svg} onClick={handleClick} ratio={ratio} />
+    <S.StyledIcon as={svg} active={active} onClick={handleClick} ratio={ratio} />
   )
 }
 
 const S = {
-  StyledIcon: styled.svg.withConfig({shouldForwardProp: (prop) => !["ratio"].includes(prop)})<{ ratio: number }>`
+  StyledIcon: styled.svg.withConfig({shouldForwardProp: (prop) => !["active", "ratio"].includes(prop)})<{ active: boolean, ratio: number }>`
     width: ${({ratio}) => 35 + 10 * ratio + 'px'};
     height: ${({ratio}) => 35 + 10 * ratio + 'px'};
-    color: ${({theme}) => theme.color.black};
+    color: ${({theme, active}) => (active ? theme.color.point["400"] : theme.color.black)};
     cursor: pointer;
-      
+
     &:hover {
         color: ${({theme}) => theme.color.point["600"]}
     }
--- src/component/video/_Controller.tsx
+++ src/component/video/_Controller.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import HighlighterSVG from "@/assets/image/highlighter.svg?react";
 import {useVideoStore} from "@/store/useVideoStore.ts";
 import CalendarSVG from "@/assets/image/calendar.svg?react";
-import {Icon} from "@/component/_common/Icon.tsx";
+import {ToggleIcon} from "@/component/_common/ToggleIcon.tsx";
 
 export const _Controller = () => {
   const { switchHighlighter } = useVideoStore();
@@ -12,8 +12,8 @@ export const _Controller = () => {
 // TODO: calendar 라이브러리
   return (
     <S.Wrapper>
-      <Icon svg={CalendarSVG} />
-      <Icon svg={HighlighterSVG} onClick={switchHighlighter} />
+      <ToggleIcon svg={CalendarSVG} />
+      <ToggleIcon svg={HighlighterSVG} onClick={switchHighlighter} />
     </S.Wrapper>
   )
 }
