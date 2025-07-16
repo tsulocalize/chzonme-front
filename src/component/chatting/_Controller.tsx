d022f6b818978f15ce1a097eb25d18cf2c08c847
tsulocalize <tsulocalizing@gmail.com>
2025-07-17 06:41:54 +0900

    feat: 채팅 페이지 달력 추가

--- src/component/chatting/_Controller.tsx
+++ src/component/chatting/_Controller.tsx
@@ -3,11 +3,38 @@
 import styled from "styled-components";
 import CalendarSVG from "@/assets/image/calendar.svg?react";
 import {ToggleIcon} from "@/component/_common/ToggleIcon.tsx";
+import {onToastError} from "@/util/alert.ts";
+import {useUserStore} from "@/store/useUserStore.ts";
+import {useState} from "react";
+import {useChattingStore} from "@/store/useChattingStore.ts";
+import {Calendar} from "@/component/_common/Calendar.tsx";
+import {useIsMobile} from "@/hook/useIsMobile.ts";
 
 export const _Controller = () => {
+  const isMobile = useIsMobile();
+  const { userChannelId, tier } = useUserStore();
+  const { date, setDate } = useChattingStore();
+  const [ activeCalendar, setActiveCalendar ] = useState(false);
+
+  const handleCalendar = () => {
+    if (!userChannelId) {
+      onToastError("로그인된 회원만 사용할 수 있습니다.");
+      return;
+    }
+    if (!tier) {
+      onToastError("1티어 이상 구독 전용 기능입니다.");
+      return;
+    }
+    setActiveCalendar(!activeCalendar);
+  }
+
   return (
     <S.Wrapper>
-      <ToggleIcon svg={CalendarSVG} />
+      <>
+        <ToggleIcon svg={CalendarSVG} onClick={handleCalendar} isMobile={isMobile}/>
+        { activeCalendar && (<Calendar date={date} setDate={setDate} />)}
+      </>
+
     </S.Wrapper>
   )
 }
