b83722fbd3aef05bc76c42c136dec0f13d2975e8
tsulocalize <tsulocalizing@gmail.com>
2025-07-24 19:29:04 +0900

    feat: 쿼리로 핀볼 url 만들기

--- src/store/useSubMenuStore.ts
+++ src/store/useSubMenuStore.ts
@@ -1,4 +1,5 @@
 import {create} from "zustand/react";
+import {useVoteStore} from "@/store/useVoteStore.ts";
 
 interface Menu {
   title: string;
@@ -24,6 +25,12 @@ const routeDataMap: Record<string, Menu[]> = {
     {title: "영도 고화질 설정", shortTitle: "고화질 설정", onClick: () => {window.open("/video-guide", "_blank")}},
   ],
   "/roulette": [
-    {title: "핀볼로 내보내기", shortTitle: "핀볼", onClick: () => {window.open("/pinball", "_blank")}},
+    {title: "핀볼로 내보내기", shortTitle: "핀볼", onClick: () => {
+      const unitPrice = useVoteStore.getState().unitPrice;
+      const baseUrl = "https://pinball.chz-on.me?names="
+      const query = useVoteStore.getState().votes.map(vote => {
+        return vote.name + '*' + String(vote.cheese / unitPrice)
+      }).join(",")
+      window.open(baseUrl + query, "_blank")}},
   ],
 }
