d294e56122f35004dea965a719e8cec2e5865ea3
tsulocalize <tsulocalizing@gmail.com>
2025-07-24 19:28:20 +0900

    feat: 채널 목록 api 구현

--- src/api/server/channel.ts
+++ src/api/server/channel.ts
@@ -1,8 +1,7 @@
 import {serverApi} from "@/api/server/_index.ts";
 
 export const getAllChannels = async () => {
-  // TODO: 서버 측 구현 필요
-  const response = await serverApi.get("/channels");
+  const response = await serverApi.get("/connection/channels");
   return response.data;
 }
 
