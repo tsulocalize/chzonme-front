d07bf8fd637851125f6e5dc90017a813057a5774
tsulocalize <tsulocalizing@gmail.com>
2025-09-01 17:04:55 +0900

    fix: 가이드 임시 중단 (floater 위치 처리 오류)

--- src/guidance/Guidance.tsx
+++ src/guidance/Guidance.tsx
@@ -1,37 +1,38 @@
-import Joyride, {type CallBackProps, STATUS} from "react-joyride";
-import {useGuidanceStore} from "@/store/useGuidanceStore.ts";
-import {useIsMobile} from "@/hook/useIsMobile.ts";
+// import {type CallBackProps, STATUS} from "react-joyride";
+// import {useGuidanceStore} from "@/store/useGuidanceStore.ts";
+// import {useIsMobile} from "@/hook/useIsMobile.ts";
 
 export const Guidance = () => {
-  const isMobile = useIsMobile();
-  const {steps, watched, setWatched} = useGuidanceStore();
-
-  const handleCallback = (data: CallBackProps) => {
-    const {status} = data;
-    if (status === STATUS.FINISHED) {
-      setWatched(true);
-    }
-  };
+  // const isMobile = useIsMobile();
+  // const {steps, watched, setWatched} = useGuidanceStore();
+  //
+  // const handleCallback = (data: CallBackProps) => {
+  //   const {status} = data;
+  //   if (status === STATUS.FINISHED) {
+  //     setWatched(true);
+  //   }
+  // };
 
   return (
-    <Joyride
-      steps={steps}
-      run={!watched}
-      continuous={true}
-      disableScrolling={true}
-      callback={handleCallback}
-      locale={{
-        back: '이전',
-        close: '닫기',
-        last: '완료',
-        next: '다음',
-        skip: '건너뛰기',
-      }}
-      styles={{
-        options: {
-          width: `${isMobile ? '250px' : ''}`
-        }
-      }}
-    />
+    // <Joyride
+    //   steps={steps}
+    //   run={!watched}
+    //   continuous={true}
+    //   disableScrolling={true}
+    //   callback={handleCallback}
+    //   locale={{
+    //     back: '이전',
+    //     close: '닫기',
+    //     last: '완료',
+    //     next: '다음',
+    //     skip: '건너뛰기',
+    //   }}
+    //   styles={{
+    //     options: {
+    //       width: `${isMobile ? '250px' : ''}`
+    //     }
+    //   }}
+    // />
+    <></>
   );
 }
