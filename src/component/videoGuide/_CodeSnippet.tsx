a5df0664f44f4bda17ff9f4a3a642ecea5790184
tsulocalize <tsulocalizing@gmail.com>
2025-09-22 00:10:26 +0900

    fix: 영상도네이션 css 수정

--- src/component/videoGuide/_CodeSnippet.tsx
+++ src/component/videoGuide/_CodeSnippet.tsx
@@ -60,34 +60,49 @@ const S = {
   `,
 }
 
-const code =
-  `body { background-color: rgba(0, 0, 0, 0); margin: 0px auto; overflow: hidden; }
-  iframe[src*="www.youtube-nocookie.com"] {
+const code = `body { background-color: rgba(0, 0, 0, 0); margin: 0px auto; overflow: hidden; }
+iframe[src*="www.youtube-nocookie.com"] {
   position: relative;
   width: 1600px !important;
   height: 900px !important;
   top: 700px;
-  }
-  iframe[title*="CHZZK Player"] {
-  position: relative;width: 1600px !important;
+}
+iframe[title*="CHZZK Player"] {
+  position: relative;
+  width: 1600px !important;
   height: 2300px !important;
   clip-path: inset(650px 0px 650px 0px);
   z-index: 1;
-  }
-  [class^="overlay_donation_alarm"] {
-  position: relative;top: -690px;
-  }
-  [class^="overlay_donation_animation"] {
+}
+[class^="overlay_donation_alarm"] {
   position: relative;
-  top: 625px;
-  }
-  [class^="overlay_donation_contents"] {
+  top: -690px;
+}
+[class^="overlay_donation_animation"] {
+  position: relative;
+  transform: scale(0.8);
+  top: 550px;
+}
+[class^="overlay_donation_contents"] {
   position: absolute;
   top: 1690px !important;
   width: 1600px !important;
   z-index: 999;
   transform: scale(1.5);
   font-size: 32px !important;
-  }
-  [class*="overlay_donation_play_video"] {font-size: 24px !important;margin-botton:20px;}
-  [class^="overlay_donation_video_title"] {margin-top: 20px !important;}`;
+}
+[class*="overlay_donation_play_video"] {
+  left: 50%;
+  transform: translateX(-50%);
+}
+[class*="overlay_donation_video_title"] {
+  margin-top: 20px !important;
+  line-height: 34px !important;
+  height: 40px !important;
+}
+[class^="overlay_donation_icon_cheese"] {
+  height: 40px !important;
+  width: 40px !important;
+  margin: -5px 2px 0 6px !important;
+}
+`
